02f1b5c1d9fbdfee5176895c7ba2b745ac253a33
Berjoyner <joyner.amber@gmail.com>
2014-05-06 14:20:44 -0400

    Initial Commit

---
+++ app/scripts/formArr.js
@@ -0,0 +1,12 @@
+var formArr = [ 
+
+{ stuff: "Code"
+},
+
+{stuff: "Coding With Beer"
+},
+
+{stuff: "Coding With Beer & Video Games"
+}
+
+];
---
+++ app/scripts/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+var newItem =_.template($('#toodleTemplate').html(), formArr);
+
+$('.putItHere').append(newItem);
+
+    // formArr = [];
+    $('input[type="button"]').click(function () {
+        var listdo = $('#listdo').val();
+        console.log(listdo);
+        
+var listdonewobject = {
+	stuff:listdo
+};
+
+formArr.push(listdonewobject);
+        console.log(formArr);
+
+var newItem =_.template($('#toodleTemplate').html(), formArr);   
+$('.putItHere').html(newItem);
+
+});
+
+
+    $('li').click(function() {
+    $(this).toggleClass('stroked');
+    $(this).find('span').toggleClass('glyphicon glyphicon-pushpin');
+
+    });
+});
+
